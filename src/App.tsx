1eb9d83eef87c15c378253508c466b9c6d973f33
Faith <faithodularu@hotmail.com>
2022-04-21 16:48:27 +0100

    fetched repository data and set repositories state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import GitHubSearch from "./components/GitHubSearch";
+
 function App() {
-  return <></>;
+  const [repositories, setRepositories] = useState();
+  const [searchTerm, setSearchTerm] = useState("tetris");
+  useEffect(() => {
+    fetch(
+      `https://api.github.com/search/repositories?q=${searchTerm}&sort=stars&order=desc&per_page=25`, //query searches for repositories with  searchTerm in the name, the description, or the README. Results are sorted by stars in descending order
+      {
+        headers: {
+          Accept: "application/vnd.github.v3+json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setRepositories(data.items);
+        console.log(data.items); //;;;;;;
+      });
+  }, []);
+  return (
+    <>
+      <GitHubSearch />
+    </>
+  );
 }
 
 export default App;
