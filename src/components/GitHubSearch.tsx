a7e31bfb27fde780f6f5b3379109c4dea6988096
Faith <faithodularu@hotmail.com>
2022-04-21 17:40:28 +0100

    created input and table, now fetching data onChange instead of in useEffect

--- src/components/GitHubSearch.tsx
+++ src/components/GitHubSearch.tsx
@@ -1,3 +1,53 @@
-export default function GitHubSearch() {
-  return <></>;
+import { IRepository } from "../interfaces/IRepository";
+import { fetchData } from "../utils/fetchData";
+
+export default function GitHubSearch({
+  searchTerm,
+  setSearchTerm,
+  repositories,
+  setRepositories,
+}: {
+  searchTerm: string;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+  repositories: IRepository[];
+  setRepositories: React.Dispatch<React.SetStateAction<IRepository[]>>;
+}) {
+  console.log(repositories);
+  return (
+    <>
+      <form id="form" role="search">
+        <input
+          value={searchTerm}
+          type="search"
+          name="q"
+          placeholder="Search..."
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+            fetchData(searchTerm, setRepositories);
+          }}
+        />
+      </form>
+
+      {repositories.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Repository Name</th>
+              <th>Author Name</th>
+              <th>Number of Stars</th>
+            </tr>
+          </thead>
+          <tbody>
+            {repositories.map((repo, i) => (
+              <tr key={i}>
+                <td>{repo.name}</td>
+                <td>{repo.owner.login}</td>
+                <td>{repo.stargazers_count}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
