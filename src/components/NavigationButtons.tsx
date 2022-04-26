983dfa0f136b4fbdb5e73ae5e698a87ee2029708
Faith <faithodularu@hotmail.com>
2022-04-26 13:37:41 +0100

    removed previous button from the first page of results

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -1,22 +1,27 @@
 export default function NavigationButtons({
+  page,
   setPage,
 }: {
+  page: number;
   setPage: React.Dispatch<React.SetStateAction<number>>;
 }) {
   return (
     <div className="buttons">
-      <button
-        onClick={() =>
-          setPage((prev) => {
-            if (prev > 1) {
-              return prev - 1;
-            }
-            return prev;
-          })
-        }
-      >
-        previous
-      </button>
+      {page !== 1 && (
+        <button
+          onClick={() =>
+            setPage((prev) => {
+              if (prev > 1) {
+                return prev - 1;
+              }
+              return prev;
+            })
+          }
+        >
+          previous
+        </button>
+      )}
+
       <button onClick={() => setPage((prev) => prev + 1)}>next</button>
     </div>
   );
